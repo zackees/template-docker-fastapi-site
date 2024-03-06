fe0ad4c01ab9c4c250aed7ac6258c52c3fadd3c0
zackees <z@zackees.com>
2024-03-05 17:20:00 -0800

    Add script to download dependencies and run development servers

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 """
+
+  curl https://raw.githubusercontent.com/zackees/install.py/main/install.py | python
   To enter the environment run:
     source activate.sh
 
@@ -11,75 +13,76 @@
 
 import argparse
 import os
+import re
 import shutil
 import subprocess
 import sys
 import warnings
 from shutil import which as find_executable
+from typing import Optional
+
+IS_GITHUB = 'GITHUB_WORKSPACE' in os.environ
 
 # This activation script adds the ability to run it from any path and also
 # aliasing pip3 and python3 to pip/python so that this works across devices.
 _ACTIVATE_SH = """
 #!/bin/bash
+set -e
 
-# Function that computes absolute path of a file
-abs_path() {
-  dir=$(dirname "$1")
-  (cd "$dir" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
-}
+# Get the directory of the current script
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Navigate to the directory where the current script resides
-bashfile=$(abs_path "${BASH_SOURCE[0]}")
-selfdir=$(dirname "$bashfile")
-cd "$selfdir"
+# Change to that directory
+cd "$script_dir"
 
-if [[ "$IN_ACTIVATED_ENV" == "1" ]]; then
+# Confirm the current directory
+echo "Changed directory to: $(pwd)"
+echo "activate.sh change working directory to $(pwd)"
+
+
+if [ "$IN_ACTIVATED_ENV" = "1" ]; then
   IN_ACTIVATED_ENV=1
 else
   IN_ACTIVATED_ENV=0
 fi
 
-if [[ "$IN_ACTIVATED_ENV" == "1" ]]; then
-  # If it is, set the variable 'IN_ACTIVATED_ENV' to true
-  IN_ACTIVATED_ENV=1
-else
-  # Otherwise, set 'IN_ACTIVATED_ENV' to false
-  IN_ACTIVATED_ENV=1
-fi
-
 # If the 'venv' directory doesn't exist, print a message and exit.
-if [[ ! -d "venv" ]]; then
-  echo "The 'venv' directory does not exist, creating..."
-  if [[ "$IN_ACTIVATED_ENV" == "1" ]]; then
-    echo "Cannot install a new environment while in an activated environment. Please launch a new shell and try again."
-    exit 1
-  fi
-  # Check the operating system type.
-  # If it is macOS or Linux, then create an alias 'python' for 'python3'
-  # and an alias 'pip' for 'pip3'. This is helpful if python2 is the default python in the system.
+if [ ! -d "venv" ]; then
+  cwd=$(pwd)
+  echo "The 'venv' directory in $cwd does not exist, creating..."
   echo "OSTYPE: $OSTYPE"
-  if [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "linux-gnu"* ]]; then
-    python3 install.py
-  else
-    python install.py
-  fi
+  case "$OSTYPE" in
+    darwin*|linux-gnu*)
+      python3 ./install.py
+      ;;
+    *)
+      python ./install.py
+      ;;
+  esac
 
   . ./venv/bin/activate
   export IN_ACTIVATED_ENV=1
+  this_dir=$(pwd)
+  export PATH="$this_dir:$PATH"
   echo "Environment created."
   pip install -e .
   exit 0
 fi
 
-. ./venv/bin/activate
+if [[ "$IN_ACTIVATED_ENV" != "1" ]]; then
+  . ./venv/bin/activate
+  export IN_ACTIVATED_ENV=1
+  this_dir=$(pwd)
+  export PATH="$this_dir:$PATH"
+fi
 """
 HERE = os.path.dirname(__file__)
 os.chdir(os.path.abspath(HERE))
-HERE = os.path.dirname(__file__)
-WWW = os.path.join(HERE, "www")
+
+print(f"install.py changed directory to {os.getcwd()}")
 
 
-def _exe(cmd: str, check: bool = True, cwd: str | None = None) -> None:
+def _exe(cmd: str, check: bool = True, cwd: Optional[str] = None, env: Optional[dict] = None) -> None:
     msg = (
         "########################################\n"
         f"# Executing '{cmd}'\n"
@@ -89,13 +92,35 @@ def _exe(cmd: str, check: bool = True, cwd: str | None = None) -> None:
     sys.stdout.flush()
     sys.stderr.flush()
     # os.system(cmd)
-    subprocess.run(cmd, shell=True, check=check, cwd=cwd)
+    kwargs = {}
+    if cwd is not None:
+        kwargs["env"] = env
+    subprocess.run(cmd, shell=True, check=check, cwd=cwd, **kwargs)
 
 
 def is_tool(name):
     """Check whether `name` is on PATH."""
 
     return find_executable(name) is not None
+
+
+def platform_ensure_python_installed() -> None:
+    try:
+        python_x = "python" if sys.platform == "win32" else "python3"
+        stdout = subprocess.check_output(
+            [python_x, "--version"], universal_newlines=True
+        )
+        print(f"Python is already installed: {stdout}")
+        return
+    except Exception:  # pylint: disable=broad-except
+        pass
+    if sys.platform == "darwin":
+        _exe("brew install python3")
+    elif sys.platform == "linux":
+        _exe("sudo apt-get install python3")
+    elif sys.platform == "win32":
+        _exe("choco install python3")
+
 
 def get_pip() -> str:
     if sys.platform == "win32":
@@ -114,20 +139,6 @@ def create_virtual_environment() -> None:
         warnings.warn(f"couldn't make virtual environment because of {exc}")
         raise exc
 
-    # _exe('python3 -m venv venv')
-    # Linux/MacOS uses bin and Windows uses Script, so create
-    # a soft link in order to always refer to bin for all
-    # platforms.
-    if sys.platform == "win32":
-        target = os.path.join(HERE, "venv", "Scripts")
-        link = os.path.join(HERE, "venv", "bin")
-        if not os.path.exists(link):
-            _exe(f'mklink /J "{link}" "{target}"', check=False)
-    with open("activate.sh", encoding="utf-8", mode="w") as fd:
-        fd.write(_ACTIVATE_SH)
-    if sys.platform != "win32":
-        _exe("chmod +x activate.sh")
-
 
 def check_platform() -> None:
     if sys.platform == "win32":
@@ -136,10 +147,24 @@ def check_platform() -> None:
             print("This script only works with git bash on windows.")
             sys.exit(1)
 
+def convert_windows_path_to_git_bash_path(path: str) -> str:
+    # Function to replace the matched drive letter and colon
+    def replace_drive_letter(match):
+        return "/" + match.group(1).lower() + "/"
+
+    # Replace the drive letter and colon with Git Bash format
+    path = re.sub(r"([A-Za-z]):\\", replace_drive_letter, path)
+    # Replace backslashes with forward slashes
+    path = path.replace("\\", "/")
+    return path
+
 
 def modify_activate_script() -> None:
     path = os.path.join(HERE, "venv", "bin", "activate")
-    text_to_add = '\nPATH="./:$PATH"\n' + "export PATH"
+    abs_path = os.path.abspath(path)
+    if sys.platform == "win32":
+        abs_path = convert_windows_path_to_git_bash_path(abs_path)
+    text_to_add = f'\nPATH="{abs_path}:$PATH"\n' + "export PATH"
     with open(path, encoding="utf-8", mode="a") as fd:
         fd.write(text_to_add)
 
@@ -151,6 +176,7 @@ def main() -> int:
             "Cannot install a new environment while in an activated environment. Please launch a new shell and try again."
         )
         return 1
+    platform_ensure_python_installed()
     parser = argparse.ArgumentParser(description="Install the project.")
     parser.add_argument(
         "--remove", action="store_true", help="Remove the virtual environment"
@@ -164,22 +190,51 @@ def main() -> int:
         create_virtual_environment()
     else:
         print(f'{os.path.abspath("venv")} already exists')
-    assert os.path.exists("activate.sh"), "activate.sh does not exist"
+    # Is this necessary?
+    os.chdir(os.path.abspath(HERE))
+    activate_sh = os.path.join(HERE, "activate.sh")
+    if not os.path.exists(activate_sh):
+      with open(activate_sh, encoding="utf-8", mode="w") as fd:
+          fd.write(_ACTIVATE_SH)
+      if sys.platform != "win32":
+          _exe(f'chmod +x {activate_sh}')
+      _exe(f'git add {activate_sh} --chmod=+x')
+
+
+
+    # Linux/MacOS uses bin and Windows uses Script, so create
+    # a soft link in order to always refer to bin for all
+    # platforms.
+    if sys.platform == "win32" and not os.path.exists(os.path.join(HERE, "venv", "bin")):
+        print("Creating soft link for windows")
+        target = os.path.join(HERE, "venv", "Scripts")
+        link = os.path.join(HERE, "venv", "bin")
+        if not os.path.exists(link):
+            _exe(f'mklink /J "{link}" "{target}"', check=False)
+
+    
+    assert os.path.exists(activate_sh), f"{activate_sh} does not exist"
     modify_activate_script()
     # Note that we can now just use pip instead of pip3 because
     # we are now in the virtual environment.
     try:
-        _exe("./activate.sh && pip install -e .")  # Why does this fail on windows git-bash?
+        if sys.platform == "win32":
+            path = os.path.join(HERE, "venv", "Scripts")
+        else:
+            path = os.path.join(HERE, "venv", "bin")
+        env = os.environ.copy()
+        env["PATH"] = f"{path}{os.pathsep}{env['PATH']}"
+        _exe("pip install -e .", env=env)  # Why does this fail on windows git-bash?
         print(
             'Now use ". ./activate.sh" (at the project root dir) to enter into the environment.'
         )
         return 0
     except subprocess.CalledProcessError:
+        if IS_GITHUB:
+            raise
         print(
-            "\n###########\n# WARNING - INSTALL NOT COMPLETE!\n"
-            "Please finish install by typing in `. ./activate.sh && pip install -e .`\n"
-            "then use `. ./activate.sh` to enter into the environment.\n"
-            " (And always use `. ./activate.sh` to enter into the environment.)\n"
+            "Now complete install with `. ./activate.sh && pip install -e .`\n"
+            "then use `. ./activate.sh` to enter into the environment."
         )
         return 0
 
--- install_dev.sh
+++
@@ -1,8 +0,0 @@
-# Warning: Out of date.
-cd $( dirname ${BASH_SOURCE[0]})
-./install
-. ./activate.sh
-python -m pip install pip --upgrade
-python -m pip install -r requirements.txt --upgrade
-python -m pip install -r requirements.testing.txt --upgrade
-python -m pip install  -e . --upgrade
